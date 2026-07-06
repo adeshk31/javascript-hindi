012ec43d9fde6a9f3924a405dfb5dceff86e9547
adeshk31 <adesh.kakade.comp.2023@vpkbiet.org>
2026-07-06 03:46:30 +0000

    string half video

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,9 @@
+//SYNTAX TO DECLARE STRING 
+//1 way :-  const gamename=new String('FreeFire')
+//2 nd way:-let name="adesh"
+let rollno=62
+let name="adesh"
+const gamename=new String('FreeFire')
+
+console.log(`My Name is ${name} and My Roll No is ${rollno}`)
+console.log(gamename[0])
