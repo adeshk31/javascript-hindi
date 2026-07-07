e6b1234a0b314e4738db8ed0f988d6d97bcdb075
adeshk31 <adesh.kakade.comp.2023@vpkbiet.org>
2026-07-07 08:12:10 +0000

    complete string & methods

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -5,7 +5,49 @@ let rollno=62
 let name="adesh"
 const gamename=new String('FreeFire')
 
-console.log(`My Name is ${name} and My Roll No is ${rollno}`)
+//console.log(`My Name is ${name} and My Roll No is ${rollno}`)
 //console.log(gamename[0])
-console.log(name.toUpperCase())
-console.log(name)
+//console.log(name.toUpperCase())
+//console.log(name)
+
+let chotastring= name.substring(0,2)
+ //console.log(chotastring)
+
+let ulta = name.slice(5)
+//console.log(ulta)
+
+let fn="adesh"
+let ln="kakade"
+let fullname=fn+ln
+console.log(`${fn} ${ln}`)
+
+
+//common string methods:-
+/*
+let str = "  Hello JavaScript  ";
+
+console.log("Original String:", str);
+
+console.log("Length:", str.length);
+
+console.log("Uppercase:", str.toUpperCase());
+
+console.log("Lowercase:", str.toLowerCase());
+
+console.log("Trim:", str.trim());
+
+console.log("Slice (0,5):", str.slice(0, 5));
+
+console.log("Replace:", str.replace("JavaScript", "World"));
+
+console.log("Includes 'Hello':", str.includes("Hello"));
+
+console.log("Index of 'Java':", str.indexOf("Java"));
+
+console.log("Starts with '  He':", str.startsWith("  He"));
+
+console.log("Ends with '  ':", str.endsWith("  "));
+
+let fruits = "apple,banana,mango";
+console.log("Split:", fruits.split(","));
+*/
