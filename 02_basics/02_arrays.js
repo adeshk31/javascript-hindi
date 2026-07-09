4203ce111800fe6216ef2f27171a8c50ba8899e2
adeshk31 <adesh.kakade.comp.2023@vpkbiet.org>
2026-07-09 15:13:40 +0000

    static array methods

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -6,4 +6,26 @@ const heros=["hulk","cap","ironman"]
 const dc_heros=["batman","superman",'flash']
 
 const all_heros=[...heros,...dc_heros] //spread method to combine arrays
-console.log(all_heros)
+//console.log(all_heros)
+//+++++++STATIC ARRAY METHODS+++++++++
+		
+//Array.isArray()	
+// Checks if a value is an array	
+// Array.isArray([1,2]) → true
+
+//Array.from()	
+// Creates an array from another iterable (like a string)	
+// Array.from("abc") → ['a','b','c']
+
+
+//Array.of()	
+// Creates an array from the given values	
+// Array.of(1,2,3) → [1,2,3]
+
+const arr=[1,2,3]
+const str="adesh"
+console.log(Array.isArray(str))//CHECKS IS IT ARRAY
+
+console.log(Array.from(arr)) //USED TO MAKE ARRAY FROM SOMETHING LIKE STRING
+
+console.log(Array.of("neha","manisha"))//used to make array of given values
