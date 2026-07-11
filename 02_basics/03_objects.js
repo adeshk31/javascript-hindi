7a6d6218d884d77459eb63ee467fb47219ec3541
adeshk31 <adesh.kakade.comp.2023@vpkbiet.org>
2026-07-11 10:36:18 +0000

    objects 1 done

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,33 @@
+//AN OBJECT IS A COLLECTION OF KEY-VALUE PAIRS USED TO STORE RELATED DATA AND FUNCTIONALITY
+
+const person={
+    name: "Adesh",
+    rollno:62,
+    sub:["cn","dbms","oops"]
+}
+//console.log(typeof person.sub)
+    let a= Object.keys(person)
+//console.log(typeof a)
+
+const car = {
+  brand: "Toyota",
+  model: "Corolla",
+  year: 2023,
+
+  start() {
+    console.log(this.brand + " is starting...");
+  }
+};
+
+//console.log(car.brand);      // Toyota
+//car.start();                 // Toyota is starting...
+
+car.color = "White";
+//console.log(car.color);      // White
+
+delete car.year;
+//console.log(Object.keys(car));
+//console.log(Object.values(car));
+//console.log(Object.entries(car));
+
+console.log(car.brand,car.model);
