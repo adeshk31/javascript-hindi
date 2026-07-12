223a274bbcd41de2fe41dd09833037993a41f330
adeshk31 <adesh.kakade.comp.2023@vpkbiet.org>
2026-07-12 08:42:19 +0000

    done with objects for now

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,14 @@
+//ANOTHER WAY TO DECLARE OBJECTS:
+//OBJECT AS A CONSTRUCTOR
+const person =new Object()
+ person.name="adesh"
+ person.rollno=62
+console.log( person)
+//ANOTHER WAY
+
+const game={}
+game.size=746
+game.name="Freefire"
+
+console.log(game)
+
